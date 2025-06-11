c70272fdb0f91000bac318ba335e50034035616c
ErikSan33 <erik_b18@msn.com>
2025-06-11 20:57:18 -0300

    Add files via upload

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,9 @@
+function Mostar ()
+{  
+
+    let elnombre= document.getElementById("elNombre").value;
+    let laEdad= document.getElementById("laEdad").value;
+
+   
+    alert ("usted se llama " + elnombre + " y tiene "+ laEdad );
+}
