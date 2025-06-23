48cb95af7fca3fe6d97eb299edaae3888c24087e
ErikSan33 <erik_b18@msn.com>
2025-06-23 20:41:01 -0300

    Create jsInstruccionSwitch(2).js

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,13 @@
+function Mostrar (){
+var mes=document.getElementById("mes").value;
+switch (mes) {
+case "Julio":
+case "Agosto":    
+    alert("Abrigate que hace frio.");
+    break;
+case "Septiembre":
+case "Octubre":
+case "Noviembre":
+case "Diciembre":   
+alert("Ya pasamos el frio, ahora calor!!!.");
+break;      
