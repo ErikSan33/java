fd3bb892cb10a829e80e9d0c9fa21600b12ebe78
ErikSan33 <erik_b18@msn.com>
2025-06-23 19:46:05 -0300

    Update jsInstruccionIF (3).js

--- jsInstruccionIF (3).js
+++ jsInstruccionIF (3).js
@@ -1,9 +1,8 @@
 function Mostrar() {
 let edad=document.getElementById("edad").value;
 if (edad>=18 )
-{ alert ("Eres mayor de edad")
-};
-if (edad<=17)
-{alert ("Eres menor de edad")};
+{ alert ("Eres mayor de edad");
+}
+else { alert ("Eres menor de edad")};
 
 }
