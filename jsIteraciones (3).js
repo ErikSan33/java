583aca913d17a14d3c7580ee55bfe7f2c8b108fe
ErikSan33 <erik_b18@msn.com>
2025-06-25 20:32:43 -0300

    Create jsIteraciones (3).js

---
+++ jsIteraciones (3).js
@@ -0,0 +1,12 @@
+function Mostrar(){
+let clave=prompt("ingrese clave");
+while(clave!="utn750"){
+    alert("clave incorrecta");
+    clave=prompt("ingrese clave nuevamente");
+}
+alert("clave correcta");
+
+
+
+
+}
